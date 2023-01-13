5e57836fe92350404e8b5a2f2fce7e3164e579f5
puhize <57593327+puhize@users.noreply.github.com>
2023-01-13 21:25:42 +0100

    Update Main.java

--- AlgorithmAnalysis/src/Main.java
+++ AlgorithmAnalysis/src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int numri = 9;
+        int numri = 10000;
         
         Main main = new Main();    
         Random rand = new Random();
